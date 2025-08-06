6dfe68df33bf615bd2b1835260c756a15c1c5bc5
Neha Yadav <ny663922@gmail.com>
2025-08-06 06:58:37 +0530

    maing watertracker layout

--- src/components/WaterTracker.jsx
+++ src/components/WaterTracker.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import '../css-components/WaterTracker.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../Redux/waterSlice';
 
 function WaterTracker() {
+    const glasses = useSelector((state) => state.water.glasses);
+    const dispatch = useDispatch();
+
+
     return (
-        <div>WaterTracker</div>
+        <div className='water-container'>
+            <h1>water Intake Tracker</h1>
+            <p>You have had <strong>{glasses}</strong> glass{glasses !== 0 && "es"} today </p>
+            <div className='btn-group'>
+                <button onClick={() => dispatch(decrement)}>-</button>
+                <button onClick={() => dispatch(increment)}>+</button>
+            </div>
+
+        </div>
     )
 }
 
